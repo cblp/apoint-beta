92adfdae7824a05555813a0a0a942228a5615122
Yuriy Syrovetskiy <cblp@cblp.su>
2014-07-23 19:29:56 +0400

    [m] style

--- Apoint/Handler/Notes.hs
+++ Apoint/Handler/Notes.hs
@@ -12,37 +12,36 @@ import GHC.Generics (Generic)
 
 
 data CreateNoteRequest = CreateNoteRequest
-  { author :: Text -- User.ident
-  , content :: Text
-  }
-    deriving (Generic)
+    { author :: Text -- User.ident
+    , content :: Text
+    }
+        deriving (Generic)
 
 instance FromJSON CreateNoteRequest
 
 
 data CreateNoteResponse = CreateNoteResponse
-  { noteId :: NoteId
-  }
-    deriving (Generic)
+    { noteId :: NoteId
+    }
+        deriving (Generic)
 
 instance ToJSON CreateNoteResponse
 
 
 postNotesR :: Handler Value
 postNotesR = do
-  $logInfo "in postNotesR"
-  parsedRequest <- parseJsonBody
-  case parsedRequest of
-    Json.Error errorDescription ->
-      invalidArgs [ "request body must be an object"
-                  , Text.pack errorDescription ]
-    Json.Success request -> do
-      -- TODO authorId <- getCurrentUser
-      let authorIdent = author request
-          noteContent = content request
-      mAuthor <- runDB $ getBy $ UniqueUser authorIdent
-      case mAuthor of
-        Nothing -> invalidArgs ["user not found", authorIdent]
-        Just (Entity authorId _) -> do
-          createdNoteId <- runDB $ insert $ Note noteContent authorId
-          returnJson CreateNoteResponse{noteId = createdNoteId}
+    parsedRequest <- parseJsonBody
+    case parsedRequest of
+        Json.Error errorDescription ->
+            invalidArgs [ "request body must be an object"
+                        , Text.pack errorDescription ]
+        Json.Success request -> do
+            -- TODO authorId <- getCurrentUser
+            let authorIdent = author request
+                noteContent = content request
+            mAuthor <- runDB $ getBy $ UniqueUser authorIdent
+            case mAuthor of
+                Nothing -> invalidArgs ["user not found", authorIdent]
+                Just (Entity authorId _) -> do
+                    createdNoteId <- runDB $ insert $ Note noteContent authorId
+                    returnJson CreateNoteResponse{noteId = createdNoteId}
