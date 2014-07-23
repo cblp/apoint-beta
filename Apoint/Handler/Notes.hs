1f14cdcbc0acefe83e26c40093e9ae450da5e57e
Yuriy Syrovetskiy <cblp@cblp.su>
2014-07-23 13:15:24 +0400

    [+] post notes

---
+++ Apoint/Handler/Notes.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE  DeriveGeneric
+            , NamedFieldPuns
+  #-}
+
+module Handler.Notes where
+
+import Import
+
+import Data.Aeson as Json
+import Data.Text as Text
+import GHC.Generics (Generic)
+
+
+data CreateNoteRequest = CreateNoteRequest
+  { author :: Text -- User.ident
+  , content :: Text
+  }
+    deriving (Generic)
+
+instance FromJSON CreateNoteRequest
+
+
+data CreateNoteResponse = CreateNoteResponse
+  { noteId :: NoteId
+  }
+    deriving (Generic)
+
+instance ToJSON CreateNoteResponse
+
+
+postNotesR :: Handler Value
+postNotesR = do
+  $logInfo "in postNotesR"
+  parsedRequest <- parseJsonBody
+  case parsedRequest of
+    Json.Error errorDescription ->
+      invalidArgs [ "request body must be an object"
+                  , Text.pack errorDescription ]
+    Json.Success request -> do
+      -- TODO authorId <- getCurrentUser
+      let authorIdent = author request
+          noteContent = content request
+      mAuthor <- runDB $ getBy $ UniqueUser authorIdent
+      case mAuthor of
+        Nothing -> invalidArgs ["user not found", authorIdent]
+        Just (Entity authorId _) -> do
+          createdNoteId <- runDB $ insert $ Note noteContent authorId
+          returnJson CreateNoteResponse{noteId = createdNoteId}
