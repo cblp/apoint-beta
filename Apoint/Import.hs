d74a09421a0b658654ff3b161da454f89bc4ec9e
Yuriy Syrovetskiy <cblp@cblp.su>
2014-07-26 16:57:58 +0400

    [-] no need in unicode operators

--- Apoint/Import.hs
+++ Apoint/Import.hs
@@ -2,33 +2,17 @@ module Import
     ( module Import
     ) where
 
-import           Prelude              as Import hiding (head, init, last,
-                                                 readFile, tail, writeFile)
-import           Yesod                as Import hiding (Route (..))
+import Control.Applicative  as Import (pure, (<$>), (<*>))
+import Control.Arrow        as Import ((>>>))
+import Data.Default         as Import (def)
+import Data.Monoid          as Import (Monoid (mappend, mempty, mconcat), (<>))
+import Data.Text            as Import (Text)
+import Prelude              as Import hiding ( head, init, last, readFile, tail
+                                             , writeFile )
+import Yesod                as Import hiding (Route (..))
 
-import           Control.Applicative  as Import (pure, (<$>), (<*>))
-import           Control.Arrow        as Import ((>>>))
-
-import           Data.Text            as Import (Text)
-
-import           Foundation           as Import
-import           Model                as Import
-import           Settings             as Import
-import           Settings.Development as Import
-import           Settings.StaticFiles as Import
-
-#if __GLASGOW_HASKELL__ >= 704
-import           Data.Monoid          as Import
-                                                 (Monoid (mappend, mempty, mconcat),
-                                                 (<>))
-#else
-import           Data.Monoid          as Import
-                                                 (Monoid (mappend, mempty, mconcat))
-
-infixr 5 <>
-(<>) :: Monoid m => m -> m -> m
-(<>) = mappend
-#endif
-
-import           Data.Default         as Import (def)
-import           Data.Monoid.Unicode  as Import ((⊕))
+import Foundation           as Import
+import Model                as Import
+import Settings             as Import
+import Settings.Development as Import
+import Settings.StaticFiles as Import
