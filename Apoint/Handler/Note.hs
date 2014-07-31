d081af12f8d8ea8978cc665c74bc4dadae8da687
Yuriy Syrovetskiy <cblp@cblp.su>
2014-07-31 18:58:19 +0400

    [+] CSRF protection on deleting note

--- Apoint/Handler/Note.hs
+++ Apoint/Handler/Note.hs
@@ -4,17 +4,30 @@ import Access
 import Import
 
 
+data NoteDelete = NoteDelete
+
+noteDeleteForm :: Html -> MForm Handler (FormResult NoteDelete, Widget)
+noteDeleteForm = renderDivs $ pure NoteDelete
+
+
 getNoteR :: NoteId -> Handler Html
 getNoteR noteId = do
     note <- runDB $ get404 noteId
     authorize Read CurrentUser note
 
     let contentHtml = noteContentHtml note
+    (noteDeleteWidget, noteDeleteEnctype) <- generateFormPost noteDeleteForm
     defaultLayout $(widgetFile "note")
 
 
 postNoteDeleteR :: NoteId -> Handler ()
 postNoteDeleteR noteId = do
+    ((formResult, _), _) <- runFormPost noteDeleteForm
+    case formResult of
+        FormSuccess NoteDelete -> return ()
+        FormMissing -> invalidArgs ["FormMissing"]
+        FormFailure errors -> invalidArgs errors
+
     note <- runDB $ get404 noteId
     authorize Delete CurrentUser note
 
