f00f697e469039d6fef495d81373797e4ca68559
Yuriy Syrovetskiy <cblp@cblp.su>
2014-08-01 10:39:26 +0400

    [m] simplofied

--- Apoint/Handler/Note.hs
+++ Apoint/Handler/Note.hs
@@ -18,12 +18,14 @@ noteDeleteForm = renderDivs $ pure NoteDelete
 
 noteSiblings ::
     [Filter Notelink] -> (Notelink -> NoteId) -> Handler [Entity Note]
-noteSiblings filters fieldForSelect = runDB $ do
-    links <- selectList filters []
-    let ids = map (entityVal >>> fieldForSelect) links
-    forM ids $ \nid -> do
-        Just n <- get nid
-        return $ Entity nid n
+noteSiblings filters fieldForSelect =
+    -- TODO remove runDB from here, replacing Handler with DB monad
+    runDB $ do
+        links <- selectList filters []
+        forM links $ \link -> do
+            let nid = fieldForSelect $ entityVal link
+            Just n <- get nid
+            return $ Entity nid n
 
 
 getNoteR :: NoteId -> Handler Html
