421482b26094c6984517625130674592aed4bf88
Yuriy Syrovetskiy <cblp@cblp.su>
2014-08-02 23:54:16 +0400

    [+] Search results as JSON

--- Apoint/Handler/Search.hs
+++ Apoint/Handler/Search.hs
@@ -1,5 +1,6 @@
 module Handler.Search where
 
+import Data.Aeson.TH
 import Data.Maybe (fromMaybe)
 
 import Local.Yesod.Auth (requireAuthId')
@@ -10,7 +11,15 @@ import Widgets.Note
 import Import
 
 
-getSearchR :: Handler Html
+data NoteSearchResult = NoteSearchResult
+    { nsr_id            :: Text
+    , nsr_content_short :: Text
+    }
+
+deriveToJSON defaultOptions{fieldLabelModifier = drop 4} ''NoteSearchResult
+
+
+getSearchR :: Handler TypedContent
 getSearchR = do
     notesOnAPage <- extraNotesOnAPage <$> getExtra
     userId <- requireAuthId'
@@ -20,6 +29,14 @@ getSearchR = do
             [NoteAuthor ==. userId, NoteContent `contains_i` query]
             [LimitTo $ notesOnAPage + 1] -- one for pagination
 
-    let title = [shamlet|Search results for <em>#{query}</em>|]
-        mode = SelectedNotes
-    defaultLayout' query $(widgetFile "noteslist") -- TODO make a function
+    selectRep $ do
+        provideRep $ do
+            let title = [shamlet|Search results for <em>#{query}</em>|]
+                mode = SelectedNotes
+            defaultLayout' query $(widgetFile "noteslist") -- TODO make a function
+        provideRep $
+            returnJson  [ NoteSearchResult
+                            { nsr_id            = toPathPiece noteId
+                            , nsr_content_short = noteContentShort note
+                            }
+                        | Entity noteId note <- notes ]
