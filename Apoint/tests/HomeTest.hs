391c99a460f9ca4f07a6a3324e2d2ea0cec88e8c
Yuriy Syrovetskiy <cblp@cblp.su>
2015-04-17 10:44:03 +0300

    [m] imports style

--- Apoint/tests/HomeTest.hs
+++ Apoint/tests/HomeTest.hs
@@ -1,10 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
-module HomeTest
-    ( homeSpecs
-    ) where
+module            HomeTest ( homeSpecs ) where
 
-import TestImport
-import qualified Data.List as L
+import            TestImport
+import qualified  Data.List as L
 
 homeSpecs :: Spec
 homeSpecs =
