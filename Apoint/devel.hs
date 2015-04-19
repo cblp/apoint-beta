561a3d5c23ddbb5b5cdc4f94314e623b905de2d0
Yuriy Syrovetskiy <cblp@cblp.su>
2015-04-19 11:14:54 +0300

    [m] style

--- Apoint/devel.hs
+++ Apoint/devel.hs
@@ -2,24 +2,22 @@
 
 import            Control.Concurrent        ( forkIO, threadDelay )
 import qualified  Network.Wai.Handler.Warp  as Warp
+import            Prelude.Extended
 import            System.Directory          ( doesFileExist, removeFile )
 import            System.Exit               ( exitSuccess )
 
-import "Apoint"   Application               ( getApplicationDev )
+import qualified  "Apoint" Application
+
 
 main :: IO ()
 main = do
     putStrLn "Starting devel application"
-    (port, app) <- getApplicationDev
+    (port, app) <- Application.getApplicationDev
     let warpSettings = Warp.setPort port Warp.defaultSettings
+
     forkIO $ Warp.runSettings warpSettings app
-    loop
 
-loop :: IO ()
-loop = do
-    threadDelay 100000
-    e <- doesFileExist "yesod-devel/devel-terminate"
-    if e then terminateDevel else loop
+    threadDelay 100000 `untilM_` doesFileExist "yesod-devel/devel-terminate"
 
-terminateDevel :: IO ()
-terminateDevel = exitSuccess
+  where
+    terminateDevel = exitSuccess
