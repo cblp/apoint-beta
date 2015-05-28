6f85a0537fb041ad92d4058200b23be89899b331
Yuriy Syrovetskiy <cblp@cblp.su>
2015-05-28 13:07:39 +0300

    [+] Prelude.Extended.bsShow, bsRead

--- src/Prelude/Extended.hs
+++ src/Prelude/Extended.hs
@@ -14,6 +14,10 @@ import Data.Monoid                as I  ( (<>), mconcat )
 import Data.Text                  as I  ( Text )
 import Prelude
 
+import            Data.ByteString       ( ByteString )
+import qualified  Data.ByteString.Char8 as ByteString
+import            Safe                  ( readMay )
+
 
 (&) :: a -> (a -> b) -> b
 x & f = f x
@@ -22,3 +26,11 @@ x & f = f x
 
 curry3 :: ((a, b, c) -> d) -> a -> b -> c -> d
 curry3 f a b c = f (a, b, c)
+
+
+-- | ByteString utilities
+bsShow :: Show a => a -> ByteString
+bsShow = ByteString.pack . show
+
+bsReadMay :: Read a => ByteString -> Maybe a
+bsReadMay = readMay . ByteString.unpack
