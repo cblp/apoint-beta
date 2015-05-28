2d25b528795079287d71ad04554c92aecff4ab72
Yuriy Syrovetskiy <cblp@cblp.su>
2015-05-28 13:06:05 +0300

    [+] requireAuth'

--- src/Yesod/Auth/Extended.hs
+++ src/Yesod/Auth/Extended.hs
@@ -1,34 +1,44 @@
 {-# LANGUAGE ConstraintKinds #-}
 
-module Yesod.Auth.Extended where
+module Yesod.Auth.Extended ( requireAuth', requireAuthId' ) where
 
+import Database.Persist ( Entity )
 import Prelude
-import Yesod      ( HandlerT, Yesod
-                  , acceptsJson
-                  , authRoute
-                  , getYesod
-                  , notAuthenticated
-                  , permissionDenied
-                  , redirect
-                  , setUltDestCurrent
-                  )
-import Yesod.Auth ( AuthId, YesodAuthPersist, maybeAuthId )
+import Yesod            ( HandlerT, Yesod
+                        , acceptsJson
+                        , authRoute
+                        , getYesod
+                        , notAuthenticated
+                        , permissionDenied
+                        , redirect
+                        , setUltDestCurrent
+                        )
+import Yesod.Auth       ( AuthEntity, AuthId, YesodAuthPersist
+                        , maybeAuth, maybeAuthId
+                        )
 
 
--- | Similar to 'requireAuthId',
+-- | Similar to "requireAuth",
+-- but returns "HTTP 401 Unauthorized" if user is not authorized.
+requireAuth' :: YesodAuthPersist master =>
+                HandlerT master IO (Entity (AuthEntity master))
+requireAuth' = maybeAuth >>= maybe handleAuthLack return
+
+
+-- | Similar to "requireAuthId",
 -- but returns "HTTP 401 Unauthorized" if user is not authorized.
 requireAuthId' :: YesodAuthPersist master => HandlerT master IO (AuthId master)
-requireAuthId' =
-    maybeAuthId >>= maybe handleAuthLack return
-    where
-        handleAuthLack = do
-            aj <- acceptsJson
-            if aj then notAuthenticated else redirectLogin
+requireAuthId' = maybeAuthId >>= maybe handleAuthLack return
 
-        redirectLogin :: Yesod master => HandlerT master IO a
-        redirectLogin = do
-            y <- getYesod
-            setUltDestCurrent
-            case authRoute y of
-                Just z -> redirect z
-                Nothing -> permissionDenied "Please configure authRoute"
+
+handleAuthLack :: YesodAuthPersist master => HandlerT master IO a
+handleAuthLack = do
+    aj <- acceptsJson
+    if aj then notAuthenticated else redirectLogin
+  where
+    redirectLogin = do
+        y <- getYesod
+        setUltDestCurrent
+        case authRoute y of
+            Just z -> redirect z
+            Nothing -> permissionDenied "Please configure authRoute"
